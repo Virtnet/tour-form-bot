70abd633a85732d81784f1d8757141ad6971bb8b
Arthur Gusein <65174305+Virtnet@users.noreply.github.com>
2025-05-31 17:14:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/submit", async (req, res) => {
-  const { name, phone, tour_date } = req.body;
+  const { name, phone, tour_details } = req.body;
 
   try {
     const browser = await puppeteer.launch({
@@ -16,10 +16,12 @@ app.post("/submit", async (req, res) => {
 
     const page = await browser.newPage();
     await page.goto("https://rocketour.co/affiliate-form/", { waitUntil: "networkidle2" });
-
-    await page.type('input[name="your-name"]', name);
-    await page.type('input[name="tel-505"]', phone);
-    await page.type('input[name="date-256"]', tour_date);
+    
+    await page.type('input[name="affiliateId"]', "242");
+    await page.type('input[name="city"]', "רומא");
+    await page.type('input[name="leadName"]', name);
+    await page.type('input[name="leadPhone"]', phone);
+    await page.type('input[name="notes"]', tour_details);
 
     await page.click('input[type="submit"]');
     await page.waitForTimeout(3000);
